51fad6b701049d66c50c7273be8aaf839757abaa
breznovic <slymarkel@gmail.com>
2024-07-25 20:22:56 +0300

    feat: add LoginPage and react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import MainPage from "./pages/MainPage/MainPage";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/main",
+    element: <MainPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+import s from "./MainPage.module.css";
+
+const MainPage = () => {
+  return <div className={s.main}>MainPage</div>;
+};
+
+export default MainPage;
