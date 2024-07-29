aa047826e0af33e2ff140d09f4646c0d6a5a2b70
evgg0r <gor.evg2100@gmail.com>
2024-07-30 01:10:52 +0700

    feat: add Todolist.tsx

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,15 @@
 import s from "./MainPage.module.css";
+import { Todolist } from "./Todolist";
 
 const MainPage = () => {
-  return <div className={s.main}>'MainPage' test</div>;
+  return (
+    <div className={s.main}>
+      <Todolist/>
+      <Todolist/>
+      <Todolist/>
+    </div>
+  );
 };
 
 export default MainPage;
+//
