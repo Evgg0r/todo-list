0ebed4ff50b2b844776c0c6c179424d08ac00d9d
evgg0r <gor.evg2100@gmail.com>
2024-07-31 21:18:57 +0700

    feat: finish lesson 1

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,11 +2,22 @@ import s from "./MainPage.module.css";
 import { Todolist } from "./Todolist";
 
 const MainPage = () => {
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: "CSS", isDone: true },
+    { id: 1, title: "JS", isDone: true },
+    { id: 1, title: "React", isDone: false },
+  ]
+
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 1, title: "XXX", isDone: false },
+    { id: 1, title: "Jentlments of fartune", idDone: true },
+  ]
+
   return (
     <div className={s.main}>
-      <Todolist/>
-      <Todolist/>
-      <Todolist/>
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="Movies" tasks={tasks2} />
     </div>
   );
 };
